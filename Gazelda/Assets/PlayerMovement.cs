f6bd0fa4738afd7f880b048d3a8c634a1d438ede
Phillip Cherganski <phil_cherg@yahoo.com>
2020-08-01 15:45:26 +0300

    Added Animations

--- Gazelda/Assets/PlayerMovement.cs
+++ Gazelda/Assets/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+		Inp
     }
 
     // Update is called once per frame

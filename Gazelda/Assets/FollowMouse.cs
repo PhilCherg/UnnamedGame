e6f43b933bf2a0c068e3abcfe8d285d87c379add
Phillip Cherganski <phil_cherg@yahoo.com>
2020-08-03 00:06:30 +0300

    Added Mouse and Colliders

---
+++ Gazelda/Assets/FollowMouse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+	public Camera camera;
+
+    void Update()
+    {
+		Vector2 mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+		transform.position = new Vector3(mousePos.x, mousePos.y, 0);
+    }
+}
--- Gazelda/Assets/Scripts/PlayerMovement.cs
+++ Gazelda/Assets/Scripts/PlayerMovement.cs
@@ -8,10 +8,12 @@ public class PlayerMovement : MonoBehaviour
 {
 	public float PlayerMoveSpeed = 5f;
     public Animator animator;
+	public GameObject mouse;
 
     private float animTimerThreshold = 0.2f;
     private float animTimer = 0f;
     private Vector3 moveDirection;
+    private Vector3 faceDirection;
 	private bool isIdle;
 	private enum idleDirection
 	{
@@ -23,32 +25,35 @@ public class PlayerMovement : MonoBehaviour
     {
 		moveDirection.x = Input.GetAxisRaw("Horizontal");
 		moveDirection.y = Input.GetAxisRaw("Vertical");
-
+		faceDirection = mouse.transform.position - transform.position;
+		Debug.Log(faceDirection);
 		animator.SetFloat("moveX", moveDirection.x);
 		animator.SetFloat("moveY", moveDirection.y);
 		animator.SetFloat("magnitude", moveDirection.magnitude);
+		animator.SetFloat("lastMoveX", faceDirection.x);
+		animator.SetFloat("lastMoveY", faceDirection.y);
 
-		if ((moveDirection.x == 1 || moveDirection.x == -1) && (moveDirection.y == 1 || moveDirection.y == -1))
-		{
-			animator.SetFloat("lastMoveX", moveDirection.x);
-			animator.SetFloat("lastMoveY", moveDirection.y);
-            animTimer = 0;
-        } else if ((moveDirection.x == 1 || moveDirection.x == -1) && (animTimer > animTimerThreshold))
-        {
-			animator.SetFloat("lastMoveX", moveDirection.x);
-			animator.SetFloat("lastMoveY", 0);
-            animTimer = 0;
-		} else if ((moveDirection.y == 1 || moveDirection.y == -1) && (animTimer > animTimerThreshold))
-		{
-			animator.SetFloat("lastMoveX", 0);
-			animator.SetFloat("lastMoveY", moveDirection.y);
-            animTimer = 0;
-        }else
-        {
-            animTimer += Time.fixedDeltaTime;
-        }
+		//if ((moveDirection.x == 1 || moveDirection.x == -1) && (moveDirection.y == 1 || moveDirection.y == -1))
+		//{
+		//	animator.SetFloat("lastMoveX", moveDirection.x);
+		//	animator.SetFloat("lastMoveY", moveDirection.y);
+		//    animTimer = 0;
+		//} else if ((moveDirection.x == 1 || moveDirection.x == -1) && (animTimer > animTimerThreshold))
+		//{
+		//	animator.SetFloat("lastMoveX", moveDirection.x);
+		//	animator.SetFloat("lastMoveY", 0);
+		//    animTimer = 0;
+		//} else if ((moveDirection.y == 1 || moveDirection.y == -1) && (animTimer > animTimerThreshold))
+		//{
+		//	animator.SetFloat("lastMoveX", 0);
+		//	animator.SetFloat("lastMoveY", moveDirection.y);
+		//    animTimer = 0;
+		//}else
+		//{
+		//    animTimer += Time.fixedDeltaTime;
+		//}
 
-    }
+	}
 
 	private void FixedUpdate()
 	{
